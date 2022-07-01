940887ff47a68b370c9926cc4ade1c675dc8d312
Li Zhang <zhangli0623janet@gmail.com>
2022-07-01 13:30:08 -0400

    create a program to use strftime method

---
+++ strftime_method.py
@@ -0,0 +1,16 @@
+"""
+This program is to represent the different format of datetime
+by using strftime method.
+"""
+
+from datetime import datetime
+
+d = datetime(2020,11,4,14,53)
+print(d)
+print(d.strftime("%Y/%m/%d %H:%M:%S"))
+print(d.strftime("%y/%B/%d %H:%M:%S %p"))
+print(d.strftime("%a, %Y %b %d"))
+print(d.strftime("%A, %Y %B %d"))
+print(d.strftime("weekday: %w"))
+print(d.strftime("Day of the year: %j"))
+print(d.strftime("Week number of the year: %U"))
